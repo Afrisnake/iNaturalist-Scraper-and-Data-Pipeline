2f6a0ed698225940cef5867a2b7746537562e302
Chris Kelly <84908213+Afrisnake@users.noreply.github.com>
2021-10-06 16:08:00 +0200

    iNaturalist login creds module

---
+++ inat_creds.py
@@ -0,0 +1,9 @@
+'''
+    Specifies the username and password required to log into the iNaturalist website.
+
+    These variables are imported into the 'inaturalist_scraper.py' script
+
+'''
+
+username = 'your_iNaturalist_username_here'
+password = 'your_iNaturalist_password_here'
